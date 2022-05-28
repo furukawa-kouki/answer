f45c91c6266894ceec51c63ca6ce592fee900990
furukawakouki <frkwkuk08[outlook.jp>
2022-05-28 14:48:16 +0900

    FizzBuzz の拡張として下記のようなパターンを API サーバーに POST します。送信されたパターンに応じて、入力を 1 から 30 としたときの出力をブラウザに表示する Web アプリケーションを完成させてください。 の回答 パターン表の取得方法が不明だが時間がないのでコミット

--- Problem1/node/index.js
+++ Problem1/node/index.js
@@ -6,7 +6,41 @@ const server = http.createServer((req, res)=>{
    console.log(queryObject.obj);
 
    // ここに処理を記述してください。
-   ans = "answer";
+   // パターン表の取得方法がわからないのでいったん手動で書く
+   // {
+   //    "obj": [
+   //      { "num": 4, "text": "fizz" },
+   //      { "num": 7, "text": "buzz" },
+   //      { "num": 8, "text": "hoge" },
+   //      { "num": 15, "text": "huga" }
+   //    ]
+   // }
+   var ans = "";
+   var result = "";
+   for(var num = 1; num < 31; num++){
+      if(num % 4 == 0){
+         result += "fizz";
+      }
+      if(num % 7 == 0){
+         result += "buzz";
+      }
+      if(num % 8 == 0){
+         result += "hoge";
+      }
+      if(num % 15 == 0){
+         result += "huga";
+      }
+      if(result == ""){
+         result += num;
+      }
+
+      ans += result;
+      if(num == 30){
+         break;
+      }
+      ans += ", "
+      result = "";
+   }
 
    res.setHeader('Access-Control-Allow-Origin', '*');
    res.setHeader('Access-Control-Request-Method', '*');
