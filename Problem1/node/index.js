c579f1c98e42773552b5799945b0fb4b102b9e88
FurukawaKouki <furukawa@ics.or.jp>
2022-06-02 17:23:03 +0900

    問題の趣旨を大きく間違えていたため修正

--- Problem1/node/index.js
+++ Problem1/node/index.js
@@ -1,53 +1,49 @@
 const http = require('http');
 const url = require('url');
 
-const server = http.createServer((req, res)=>{
-   const queryObject = url.parse(req.url, true).query;
-   console.log(queryObject.obj);
+const server = http.createServer((req, res) => {
+    const queryObject = url.parse(req.url, true).query;
+    console.log(queryObject.obj);
 
-   // ここに処理を記述してください。
-   // パターン表の取得方法がわからないのでいったん手動で書く
-   // {
-   //    "obj": [
-   //      { "num": 4, "text": "fizz" },
-   //      { "num": 7, "text": "buzz" },
-   //      { "num": 8, "text": "hoge" },
-   //      { "num": 15, "text": "huga" }
-   //    ]
-   // }
-   var ans = "";
-   var result = "";
-   for(var num = 1; num < 31; num++){
-      if(num % 4 == 0){
-         result += "fizz";
-      }
-      if(num % 7 == 0){
-         result += "buzz";
-      }
-      if(num % 8 == 0){
-         result += "hoge";
-      }
-      if(num % 15 == 0){
-         result += "huga";
-      }
-      if(result == ""){
-         result += num;
-      }
+    // ここに処理を記述してください。
+    // パターン表の取得方法がわからないのでいったん手動で書く
+    var obj = [
+        ["4", "fizz"],
+        ["7", "buzz"],
+        ["8", "hoge"],
+        ["15", "huga"]
+    ]
 
-      ans += result;
-      if(num == 30){
-         break;
-      }
-      ans += ", "
-      result = "";
-   }
 
-   res.setHeader('Access-Control-Allow-Origin', '*');
-   res.setHeader('Access-Control-Request-Method', '*');
-   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
-   res.setHeader('Access-Control-Allow-Headers', '*');
-   res.writeHead(200, {'Content-Type': 'text/html'});
-   res.write(ans);
-   res.end();
+
+    var result = "";
+    var ans = "";
+    for (var num = 1; num < 31; num++) {
+
+        obj.forEach(function(row) {
+            if (num % Number(row[0]) == 0) {
+                result += row[1];
+            }
+        });
+
+        if (result == "") {
+            result = String(num);
+        }
+
+        ans += result;
+        if (num == 30) {
+            break;
+        }
+        ans += ", "
+        result = "";
+    }
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(ans);
+    res.end();
 });
-server.listen(8080); 
+server.listen(8080);
